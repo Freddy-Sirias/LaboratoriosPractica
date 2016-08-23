c4cb90574b61e52624abe28631dda88da15abe61
Freddy-Sirias <fsiriasmiranda@gmail.com>
2016-08-23 15:08:26 -0600

    Laboratorios clase practica

---
+++ Lab2/js/codigos.js
@@ -0,0 +1,11 @@
+//Llamamos a la funcion ready para cargar el DOM
+$(document).on("ready",inicio);
+
+function inicio(){
+     //al hacer click en el boton publicar llama ala funcion
+	$("#publicar").on("click",mensaje);
+}
+
+function mensaje(){
+	alert ("Haz precionado el boton publicar");
+}
